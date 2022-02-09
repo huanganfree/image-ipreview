91c29302e296834ce18a33faafbe9f87040a08c5
huanganfree <huangan1206@163.com>
2022-02-09 17:59:11 +0800

    chore: check-commitMsg.js添加代码注释

--- scripts/check-commitMsg.js
+++ scripts/check-commitMsg.js
@@ -1,9 +1,9 @@
 /**
- * 在使用chalk库更改git shell输出文字颜色之前，需要执行：FORCE_COLOR=1
- * 
+ * 在使用chalk库更改git shell输出文字颜色之前，需要执行：FORCE_COLOR=1(1,2,3 三个值)
+ * FORCE_COLOR: #http://nodejs.cn/api/cli/force_color_1_2_3.html
  */
 
-const chalk = require('chalk')
+const chalk = require('chalk') // 彩色输出库
 
 const msgPath = process.env.HUSKY_GIT_PARAMS // msgPath => .git/COMMIT_EDITMSG（文件存的是提交的msg）
 const msg = require('fs')
